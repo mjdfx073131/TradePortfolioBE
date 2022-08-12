182f70a55af5d157af7c37aa309d459d4c124da2
LinlinlinlinW <wangbolin9827@gmail.com>
2022-08-12 16:21:45 +0000

    setup database v1

--- sql/createTable.sql
+++ sql/createTable.sql
@@ -1,5 +1,6 @@
---DROP TABLE `Trade`.`orders`;
-
+DROP TABLE `Trade`.`orders`;
+CREATE database if not exists `Trade`;
+use `Trade`;
 CREATE TABLE `Trade`.`orders` (
   `orderId` INT NOT NULL AUTO_INCREMENT,
   `SIN` VARCHAR(45) NOT NULL,
@@ -9,10 +10,11 @@ CREATE TABLE `Trade`.`orders` (
   `status_code` INT NOT NULL,
   PRIMARY KEY (`orderId`));
 
-insert into `Trade`.`orders` values(0,'943999999','TSLA',10,879.32,0);
+
 insert into `Trade`.`orders` values(1,'943999999','MSFT',100,280.15,0);
 insert into `Trade`.`orders` values(2,'943999999','GOOGL',10,120.13,0);
 insert into `Trade`.`orders` values(3,'943999999','AMZN',4,141.53,0);
 insert into `Trade`.`orders` values(4,'943999000','COIN',10,88.19,0);
 insert into `Trade`.`orders` values(5,'943999000','MCD',10,261.68,0);
 insert into `Trade`.`orders` values(6,'943999000','C',10,54.045,0);
+insert into `Trade`.`orders` values(7,'943999999','TSLA',10,879.32,0);
