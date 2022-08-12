3b7e8d6ee0291ec3abcde135f7d554fcbc314802
Kerry <q2k2b@ugrad.cs.ubc.ca>
2022-08-12 12:04:08 -0400

    template

---
+++ src/main/java/com/trade/AppConfig.java
@@ -0,0 +1,20 @@
+package com.trade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+
+
+
+@SpringBootApplication
+@Import(SwaggerConfig.class)
+@ComponentScan // required in order for tests to pick up @Components
+public class AppConfig {
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfig.class, args);
+    }
+
+
+}
+
