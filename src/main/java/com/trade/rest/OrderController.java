d920e1d8bec1e2be8ef2d3f95287a3c2d8887c8d
juliannazhou <yianzzsh@icloud.com>
2022-08-18 17:44:19 +0000

    add 404 not found

--- src/main/java/com/trade/rest/OrderController.java
+++ src/main/java/com/trade/rest/OrderController.java
@@ -17,35 +17,39 @@ public class OrderController {
 
     @Autowired
     private OrderService orderService;
+
     @ApiOperation(value = "findAll", nickname = "findAll")
     @RequestMapping(method = RequestMethod.GET)
     public Iterable<Order> findAll() {
         return orderService.getAllOrders();
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
-    public Order getOrderById(@PathVariable("id") int id) {
-        return orderService.getOrderById(id);
-    }
 
-    @RequestMapping(method=RequestMethod.GET, value="/404/{id}")
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
     public ResponseEntity<Order> getByIdWith404(@PathVariable("id") int id) {
         Order order = orderService.getOrderById(id);
         if (order == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        else {
+        } else {
             return new ResponseEntity<>(order, HttpStatus.OK);
         }
     }
 
     @GetMapping("/SIN")
     public ResponseEntity<List<Order>> getOrdersBySIN(@RequestParam String SIN) {
-        return new ResponseEntity<List<Order>>(orderService.findBySIN(SIN), HttpStatus.OK);
+        if (orderService.findBySIN(SIN).isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<List<Order>>(orderService.findBySIN(SIN), HttpStatus.OK);
+        }
     }
 
     @GetMapping("/ticker")
-    public ResponseEntity<List<Order>> getLaptopsByBrand(@RequestParam String ticker) {
-        return new ResponseEntity<List<Order>>(orderService.findByTicker(ticker), HttpStatus.OK);
+    public ResponseEntity<List<Order>> getOrdersByTicker(@RequestParam String ticker) {
+        if (orderService.findByTicker(ticker).isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<List<Order>>(orderService.findByTicker(ticker), HttpStatus.OK);
+        }
     }
 }
