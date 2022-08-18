6d2c5f254205dc0768cca57d1d014b2bc0b42975
juliannazhou <yianzzsh@icloud.com>
2022-08-18 16:23:22 +0000

    create get methods for id, SIN, and ticker

--- src/main/java/com/trade/rest/OrderController.java
+++ src/main/java/com/trade/rest/OrderController.java
@@ -4,10 +4,11 @@ import com.trade.entities.Order;
 import com.trade.services.OrderService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/orders")
@@ -20,5 +21,31 @@ public class OrderController {
     @RequestMapping(method = RequestMethod.GET)
     public Iterable<Order> findAll() {
         return orderService.getAllOrders();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
+    public Order getOrderById(@PathVariable("id") int id) {
+        return orderService.getOrderById(id);
+    }
+
+    @RequestMapping(method=RequestMethod.GET, value="/404/{id}")
+    public ResponseEntity<Order> getByIdWith404(@PathVariable("id") int id) {
+        Order order = orderService.getOrderById(id);
+        if (order == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        else {
+            return new ResponseEntity<>(order, HttpStatus.OK);
+        }
+    }
+
+    @GetMapping("/SIN")
+    public ResponseEntity<List<Order>> getOrdersBySIN(@RequestParam String SIN) {
+        return new ResponseEntity<List<Order>>(orderService.findBySIN(SIN), HttpStatus.OK);
+    }
+
+    @GetMapping("/ticker")
+    public ResponseEntity<List<Order>> getLaptopsByBrand(@RequestParam String ticker) {
+        return new ResponseEntity<List<Order>>(orderService.findByTicker(ticker), HttpStatus.OK);
     }
 }
--- src/main/java/com/trade/services/OrderService.java
+++ src/main/java/com/trade/services/OrderService.java
@@ -7,4 +7,10 @@ import java.util.List;
 public interface OrderService {
     // get all
     List<Order> getAllOrders();
+
+    Order getOrderById(Integer orderId);
+
+    List<Order> findBySIN(String SIN);
+    List<Order> findByTicker(String ticker);
+
 }
